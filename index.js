a927d5698d88f3329e081cfd98c995ff799cd18f
kaankoylu <ekankoylu@outlook.com>
2023-09-11 11:59:08 +0200

    2.0
    content changes and css corrections

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const profile_button =  document.getElementById("profile_button");
 const dashboard_button = document.getElementById("dashboard_button");
 const faq_button =  document.getElementById("faq_button");
-const blog_button = document.getElementById("blog_button")
+const blog_button = document.getElementById("blog_button");
+const main_page = document.getElementById("main_page");
+
 
 profile_button.addEventListener("click" , () => {
         
-        
+        main_page.innerHTML=""
         let parent_element_profile = document.getElementById("profile_card");
+
         parent_element_profile.classList.toggle("active");
+
+        
 
         if(parent_element_profile.classList.contains("active")){
             let child_update  = `
@@ -45,6 +50,7 @@ dashboard_button.addEventListener("click" , () => {
 
         let parent_element_dashboard = document.getElementById("dashboard_card");
         parent_element_dashboard.classList.toggle("active");
+        main_page.innerHTML=""
 
         if(parent_element_dashboard.classList.contains("active")){
             let child_update  = `
@@ -52,43 +58,102 @@ dashboard_button.addEventListener("click" , () => {
             <table border="1">
             <tr>
 
-                <td>Program- & Career Orientation (PCO)</td>
-                <td>Program- & Career Orientation (PCO)</td>
-                <td> Computer Science Basics (CBA)</td>
-                <td>Programming Basics (PBA)</td>
-                <td>Personal professional development: Exploration (PPD-E)</td>
-                <td>IT Personality Projectweek 1 (PPW1) (PBA)</td>
-                <td>IT Personality 1 (ITP1) (PBA)</td>
-                <td></td>
-                <td></td>
-                <td>Object-oriented programming (OOP) (PBA)</td>
-                <td>Framework development 1 (FDE1) (PBA)</td>
-                <td>Framework Project 1 (FPR1)  (PBA)</td>
-                <td>Framework project 2 (FPR2) (PBA)</td>
-                <td>IT Personality International week (PIW)  (PBA)</td>
-                <td>IT Personality2 (ITP2) (PBA)</td>
-                <td>--------- end of the Semester 2 ---------</td>
-
+                <td>Quartile</td>
+                <td>Course</td>
+                <td>EC</td>
+                <td>Exams</td>
+                <td>Grade</td>
             </tr>
             <tr>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td></td>
-                <td></td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
+            <td rowspan="3">1</td>
+                <td>Program & Career Oriantation</td>
+                <td>2.5</td>
                 <td>0</td>
                 <td>0</td>
                 
+                
             </tr>
-        </table>
+            <tr>
+                <td>Computer Science Basics</td>
+                <td>5</td>
+                <td></td>
+                <td>5.5</td>
+            </tr>
+            <tr>
+
+            <td>Programming Basics</td>
+                <td>5</td>
+                <td></td>
+                <td>5.5</td>
+            </tr>
+            <tr>
+            <td rowspan="9">2</td>
+                <td>The Object-Orianted programming</td>
+                <td>10</td>
+                <td></td>
+                <td>5.5</td> 
+            
+            </tr>
+            <tr>
+                <td>Framework Project</td>
+                <td>1.25</td>
+                <td></td>
+                <td>5.5</td> 
+            
+            </tr>
+            <tr>
+                <td>Framework Project 2</td>
+                <td>10</td>
+                <td></td>
+                <td>5.5</td> 
+            
+            </tr>
+            <tr>
+                <td>Business IT consultancy Basics</td>
+                <td>2.5</td>
+                <td></td>
+                <td>5.5</td> 
+            
+            </tr>
+            <tr>
+                <td>PPDE</td>
+                <td>12.5</td>
+                <td></td>
+                <td>5.5</td> 
+            
+            </tr>
+            <tr>
+            <td>IT personality project week 1</td>
+            <td>1.25</td>
+            <td></td>
+            <td>5.5</td> 
+    
+            </tr>
+            </tr>
+            <tr>
+            <td>IT personality international week</td>
+            <td>1.25</td>
+            <td></td>
+            <td>5.5</td> 
+    
+            </tr>
+            <tr>
+            <td>IT personality 1</td>
+            <td>1.25</td>
+            <td></td>
+            <td>5.5</td> 
+    
+            </tr>
+            <tr>
+            <td>IT personality 2</td>
+            <td>1.25</td>
+            <td></td>
+            <td>5.5</td> 
+    
+            </tr>
+
+
+            </table>
 
             <p><h1>The values are temprory</h1></p> 
 
@@ -107,7 +172,7 @@ faq_button.addEventListener("click" , () => {
     
         let parent_element_faq = document.getElementById("faq_card");
         parent_element_faq.classList.toggle("active");
-
+        main_page.innerHTML=""
         if(parent_element_faq.classList.contains("active")){
             let child_update  = `
             
@@ -151,10 +216,9 @@ faq_button.addEventListener("click" , () => {
 
 document.getElementById("blog_button").addEventListener("click" , () => {
 
-
     let parent_element_blog = document.getElementById("blog_card");
     parent_element_blog.classList.toggle("active");
-
+    main_page.innerHTML=""
     if(parent_element_blog.classList.contains("active")){
         let child_update  = `
 
